c1c6ffcb4b9d28ec1f67397bcc0f86ee3fd26bd6
mattie <m_alrod@cda055ev5718x.local>
2018-10-25 14:10:58 -0400

    started week7 exercise, made some new files

---
+++ Exercises/Ex4/EX-WEEK-7/mattiesPHPEx.php
@@ -0,0 +1,11 @@
+<!-- ~~~~~~~~~~~~~~~~~~~~~~ H T M L     S E T U P ~~~~~~~~~~~~~~~~~~~~~~~~ -->
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>USING JQUERY AND AJAX AND CANVAS </title>
+
+<!-- get JQUERY -->
+  <script src = "libs/jquery-3.3.1.min.js"></script>
+
+    </head>
+  </html>
