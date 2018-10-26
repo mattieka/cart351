c699e22b1caa42ee73275f70f1a9c8a2ea422b2c
Mattie <m_alrod@cda055ev5718x.local>
2018-10-26 13:16:58 -0400

    worked on ex4 a little

--- Exercises/Ex4/EX-WEEK-7/mattiesPHPEx.php
+++ Exercises/Ex4/EX-WEEK-7/mattiesPHPEx.php
@@ -1,11 +1,35 @@
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ P H P  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+<? php
+
+
+
+?>
+
 <!-- ~~~~~~~~~~~~~~~~~~~~~~ H T M L     S E T U P ~~~~~~~~~~~~~~~~~~~~~~~~ -->
 <!DOCTYPE html>
   <html>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ H E A D ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
     <head>
       <title>USING JQUERY AND AJAX AND CANVAS </title>
 
-<!-- get JQUERY -->
+<!-- get JQUERY/external script/css -->
   <script src = "libs/jquery-3.3.1.min.js"></script>
+  <script src = "script.js"></script>
+  <link rel = "stylesheet" href = "style.css">
 
     </head>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ B O D Y  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+    <body>
+
+<!-- create the canvas -->
+<canvas id="canvas" width=500 height =500></canvas>
+
+
+    </body>
+
   </html>
---
+++ Exercises/Ex4/EX-WEEK-7/script.js
@@ -0,0 +1,21 @@
+//dont run the script until the document is ready!
+$(document).ready (function(){
+
+// --------------------------- V A R I A B L E S --------------------------- //
+
+// -------------------------------- D R A W -------------------------------- //
+
+// --------------------------- F U N C T I O N S --------------------------- //
+
+
+
+
+
+});
+
+
+//brainstorming
+
+/** ok so i need 2 clickable objects in the canvas and then the button along
+the top. and pressing those things needs to send data to php and then the
+script uses that data to change smth on the canvas
